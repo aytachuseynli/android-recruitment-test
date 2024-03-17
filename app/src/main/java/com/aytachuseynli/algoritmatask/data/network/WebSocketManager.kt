748dc2d167b7d7074f05aa26ff8397378bd94afd
aytachuseynli <aytac_ha@mail.ru>
2024-03-17 14:27:44 +0400

    ADDED WebSocketManager codes

--- app/src/main/java/com/aytachuseynli/algoritmatask/data/network/WebSocketManager.kt
+++ app/src/main/java/com/aytachuseynli/algoritmatask/data/network/WebSocketManager.kt
@@ -7,5 +7,18 @@ import javax.inject.Inject
 @ActivityScoped
 class WebSocketManager @Inject constructor(private val client: OkHttpClient) {
 
+    private var webSocket: WebSocket? = null
 
+    fun connect(url: String, listener: WebSocketListener) {
+        val request = Request.Builder().url(url).build()
+        webSocket = client.newWebSocket(request, listener)
+    }
+
+    fun disconnect() {
+        webSocket?.close(1000, "Disconnecting")
+    }
+
+    fun sendMessage(message: String) {
+        webSocket?.send(message)
+    }
 }
